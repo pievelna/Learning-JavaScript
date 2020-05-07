47a5e3ba4fbecf884ae41cee9e13a134dbef1909
Roberts Cacus <kas.pie.velna@gmail.com>
2020-05-07 23:07:27 +0300

    chess board challange

---
+++ chess_board.js
@@ -0,0 +1,29 @@
+// chess board
+
+for (var n=0; n<8; n++) {
+  var chesBoard = "";
+  if (n % 2 == 0) {
+    output = "# # # # ";
+  } else {
+    output = " # # # #";
+  }
+  console.log(output);
+}
+
+
+var size = 8;
+chessBoard = "";
+
+for (x=0;x<size;x++) {
+  for (y=0;y<size;y++) {
+    if ((x + y) % 2 == 0) {
+      chessBoard += " ";
+    } else {
+      chessBoard += "#";
+    }
+  }
+} 
+
+chessBoard += "\n";
+
+console.log(chessBoard);
