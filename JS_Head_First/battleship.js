1c19fbee420e43f26669dcdf5af93e163518e47c
rcs <roberts.cacus@eazybi.com>
2021-04-18 11:39:14 +0300

    condition to get a valid number as guess

--- JS_Head_First/battleship.js
+++ JS_Head_First/battleship.js
@@ -10,7 +10,7 @@ var isSunk = false;
 
 while (isSunk == false) {
   guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-  if (guess < 0 || guess > 6) {
+  if (isNaN(guess) || guess < 0 || guess > 6) {
     alert("Please enter a valid cell number!");
   } else {
     guesses = guesses + 1;
