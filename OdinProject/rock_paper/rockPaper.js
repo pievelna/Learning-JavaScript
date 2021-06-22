c5a9622ae7cc9b09469716f58e2abaad3df83def
rcs <roberts.cacus@eazybi.com>
2021-06-23 00:54:37 +0300

    added round and final score

--- OdinProject/rock_paper/rockPaper.js
+++ OdinProject/rock_paper/rockPaper.js
@@ -19,21 +19,20 @@ function computerPlay() {
 function userPlay() {
   // check if correct input (any case), put everything in lower case
   let userValue = prompt('Rock-paper-scissors?');
-  userValue = normalizeString(userValue);
-  if (acceptedValues.indexOf(userValue) > -1) {
-    return userValue;
+  let normalValue = normalizeString(userValue);
+  if (userValue && acceptedValues.indexOf(normalValue) > -1) {
+    return normalValue;
   } else {
     alert("We don't do that here. Try again and specify a correct value");
   }
 }
 
 function playRound(playerSelection,computerSelection) {
-  console.log(playerSelection,computerSelection);
   // compare user and computer result and determine the result
   if (playerSelection && computerSelection) {
-    let roundResult = "It is a tie! Computer also chose " + computerSelection + "."; // default value
-    const playerWon = 'You won! ' + playerSelection + ' beats ' + computerSelection + '.';
-    const computerWon = 'The computer won! ' + computerSelection + ' beats ' + playerSelection + '.';
+    let roundResult = [0, playerSelection, computerSelection]; //"It is a tie! Computer also chose " + computerSelection + "."; // default value
+    const playerWon = [1, playerSelection, computerSelection]; //'You won! ' + playerSelection + ' beats ' + computerSelection + '.';
+    const computerWon = [2, playerSelection, computerSelection];//'The computer won! ' + computerSelection + ' beats ' + playerSelection + '.';
     if (playerSelection !== computerSelection) { //if true, default result
       if (playerSelection !== 'Rock') {
         if (computerSelection === 'Scissors') {
@@ -57,14 +56,43 @@ function playRound(playerSelection,computerSelection) {
 
 // playRound(userPlay(),computerPlay());
 
+function finalResult(playerScore,computerScore) {
+  if (playerScore > computerScore) {
+    return 'You won! The final result:\n You: ' + playerScore + ' Computer: ' + computerScore + '.';
+  }
+  if (playerScore < computerScore) {
+    return 'The computer won! The final result:\n You: ' + playerScore + ' Computer: ' + computerScore + '.';
+  } else {
+    return 'It is a tie! The final result:\n You: ' + playerScore + ' Computer: ' + computerScore + '.';
+  }
+}
 
 function game() {
+  let playerScore = 0;
   let computerScore = 0;
-  let yourScore = 0
-  for (i=1;i<=5;i++) {
-    let test = playRound(userPlay(),computerPlay());
-    alert('Round ' + i + ' results - ' + test);
+  let roundScore;
+  let startGame = confirm('Want to play a game of rock-paper-scissors?');
+  if (startGame) {
+    for (i=1;i<=5;i++) {
+      let roundResult = playRound(userPlay(),computerPlay());
+      let roundNo = 'Round ' + i + ' results: ';
+      let announcement = 'Something went wrong in the round ' + i + ".";
+      if (roundResult !== undefined) {
+        if (roundResult[0] === 0) {
+          announcement = 'It is a tie! Computer also chose ' + roundResult[1] + '.';
+        } else if (roundResult[0] === 1) {
+          announcement = 'You won! ' + roundResult[1] + ' beats ' + roundResult[2] + '.';
+          playerScore++;
+        } else if (roundResult[0] === 2) {
+          announcement = 'The computer won! ' + roundResult[2] + ' beats ' + roundResult[1] + '.';
+          computerScore++;
+        }
+      }
+      roundScore = 'Player: ' + playerScore + '\nComputer: ' + computerScore;
+      alert(roundNo + announcement + '\n' + roundScore);
+    }
   }
+  alert(finalResult(playerScore,computerScore));
 }
 
 game();
