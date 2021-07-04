b4bd66fd6c3f231b08837b4e6788bcec4b42835a
pievelna <kas.pie.velna@gmail.com>
2021-07-04 15:52:25 +0300

    dom excercises

---
+++ OdinProject/domExcercises/test.js
@@ -0,0 +1,16 @@
+// your javascript file
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+
+// a <p> with red text that says “Hey I’m red!”
+// an <h3> with blue text that says “I’m a blue h3!”
+// a <div> with a black border and pink background color with the following elements inside of it:
+// another <h1> that says “I’m in a div”
+// a <p> that says “ME TOO!”
+// Hint for this one: after creating the div with createElement, append the <h1> and <p> to it before adding it to the container.
