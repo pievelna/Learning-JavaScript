5c1ae0b6fa342a772531455ed8d08e563e20a82b
rcs <roberts.cacus@eazybi.com>
2021-07-18 16:05:47 +0300

    adding html elements with javascript

--- OdinProject/domExcercises/test.js
+++ OdinProject/domExcercises/test.js
@@ -14,14 +14,21 @@ const blueH = document.createElement('h3');
 blueH.style.cssText = 'color: blue';
 blueH.textContent = "I'm a blue h3!";
 
+// a <div> with a black border and pink background color with the following elements inside of it:
+const anotherDiv = document.createElement('div');
+anotherDiv.classList.add('anotherDiv');
+anotherDiv.style.cssText='background: pink; border-style: solid';
+  // another <h1> that says “I’m in a div”
+  const hInDiv = document.createElement('h1');
+  hInDiv.textContent = "I'm in a div";
+  // a <p> that says “ME TOO!”
+  const pInDiv = document.createElement('p');
+  pInDiv.textContent = "ME TOO!";
+  // Hint for this one: after creating the div with createElement, append the <h1> and <p> to it before adding it to the container.
+
 container.appendChild(content);
 container.appendChild(redP);
 container.appendChild(blueH);
-
-
-
-
-// a <div> with a black border and pink background color with the following elements inside of it:
-// another <h1> that says “I’m in a div”
-// a <p> that says “ME TOO!”
-// Hint for this one: after creating the div with createElement, append the <h1> and <p> to it before adding it to the container.
+container.appendChild(anotherDiv);
+anotherDiv.appendChild(hInDiv);
+anotherDiv.appendChild(pInDiv);
